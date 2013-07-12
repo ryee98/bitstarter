47ecbfe01631bf6b21fb62475427296355b36c76
Richard Yee <richard.k.yee@gmail.com>
2013-07-12 03:38:58 +0000

    Changed output string

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 var fs = require('fs');
 var infile = "index.html";
 var buf = new Buffer(1024);
-var content = fs.readFile(infile);  
+var content = fs.readFileSync(infile);  
 //console.log("Script: " + __filename + "\nRead: " + buf);
 var app = express.createServer(express.logger());
 
